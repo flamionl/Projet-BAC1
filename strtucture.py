2b7e1906d6342e04c0669235bb2c4fa402915947
mat2905h <57899174+mat2905h@users.noreply.github.com>
2020-02-28 01:08:38 -0800

    Add files via upload
    Exemple de structure

---
+++ strtucture.py
@@ -0,0 +1,30 @@
+# Structure of the project
+
+    # Data structures
+board = {(0,0) : ['cruiser_1', 'hub_1'], (0,1) : ['energy_1'], (0,2) : ['tanker_1']}
+
+entities = {
+'cruiser_1': {'coordinates' : (0,0), 'type' : 'cruiser', 'team' : 'blue', 'structure_points' : 12, 
+             'available_energy' : 240, 'moving_cost': 10, 'fire_range': 1},
+'hub_1': {'coordinates' : (0,0), 'type' : 'hub', 'team' : 'blue', 'structure_points' : 2000, 
+               'available_energy' : 622, 'regeneration_rate' : 25},
+'tanker_1' : {'coordinates' : (0,2), 'type' : 'tanker', 'team' : 'red', 'storage_capacity': 600, 
+                'available_energy': 300, 'structure_points': 50},
+'energy_1' : {'coordinates' : (0,1, 'type' : 'energy', 'value' : 25},
+'energy_2' : {'coordinates' : (0,2), 'type' : 'energy', 'value' : 40}
+}
+
+     # Scenario : moving a cruiser
+
+        # Changing coordinates in vehicles dict
+
+entities['cruiser_1']['coordinates'] = [0,1]
+
+        # deleting cruiser from old coordinates in board dict
+index = board[(0,0)].index('cruiser_1')
+del board[(0,0)][index]
+
+        # Adding cruiser in new coordinates in board dict
+board[(0,1)].append('cruiser_1')
+
+print(board)
