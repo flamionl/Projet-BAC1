1519dc619f9b5db353f3cdf954add0b7f91a3bc9
Van Pevenaeyge <avanpevenaeyge@outlook.fr>
2020-03-19 17:00:51 +0100

    Quelques petits changements pour algo IA
    Idées à vérifier

--- Algo_IA.py
+++ Algo_IA.py
@@ -11,38 +11,84 @@ attack_cruisers = []
 defense_cruisers = []
 turn = 0
 
+#FIRST PHASE
+
 while regeneration_rate < 50:
     create tanker
     if turn is pair:
         add tanker to attack_tankers
+
     else:
         add tanker to refuel_tankers
+
+    upgrade regeneration_rate of the hub
+
     for tanker in entities:
         move tankers towards different peaks
+
         for peaks in entities:
             if distance between tanker and peak <= 1:
                 absorb energy from the peak
         end for
+
         if distance between tanker and hub <= 1:
             transfer all the energy from the tanker to the hub
     end for
-    upgrade regeneration_rate of the hub
+
     turn += 1
+
 end while
+
+#SECOND PHASE
 
 while moving_cost > 5 and regeneration rate == 50:
     create cruiser
     add cruiser to attack_cruisers
+    upgrade the moving cost of the cruisers
+
     for tanker in refuel_tankers:
+
         if tanker available_energy > 0:
             move tanker towards hub
+
             if distance between tanker and hub <= 1:
                 transfer all the energy from the tanker to the hub
+
+            end if
+        end if
+
         else:
             move tanker towards different peaks
-ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+            #il faudrait parcourir tous les peaks et vérifier quel tanker en est le plus proche pour bien répartir les transferts d'énergie
+
             if distance between tanker and peak <= 1:
                 absorb energy from the peak
+
+            end if
+        end else
+    end for
             
-    elif entity is a cruiser:
-        move cruiser towards the opponent but on a case where there is no other cruiser from our team
+    for tanker in attack_tankers:
+
+        if tanker available_energy > 0:
+            move tanker towards the closest cruiser
+
+            if distance between tanker and the closest cruiser <= 1:
+                transfer all the energy from the tanker to the cruiser
+
+            end if
+        end if
+
+        else:
+            move tanker towards different peaks
+            #il faudrait parcourir tous les peaks et vérifier quel tanker en est le plus proche pour bien répartir les transferts d'énergie
+
+            if distance between tanker and peak <= 1:
+                absorb energy from the peak
+
+            end if
+        end else
+    end for
+
+#THIRD PHASE
+
