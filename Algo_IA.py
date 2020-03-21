4f684e1ce81753b2eff662979a7537c22324eb41
Mathis Huet <mathis.huet@student.unamur.be>
2020-03-21 11:58:18 +0100

    début algo

--- Algo_IA.py
+++ Algo_IA.py
@@ -4,91 +4,106 @@
 input : two dictionnaries composing our data structure board and entities
 output : chain of characters containing orders for the game
 
-attack_tankers = []
-defense_tankers = []
-refuel_tankers = []
-attack_cruisers = []
-defense_cruisers = []
-turn = 0
-
 #FIRST PHASE
 
-while regeneration_rate < 50:
+while regeneration_rate < 50
+
     create tanker
-    if turn is pair:
-        add tanker to attack_tankers
-
-    else:
-        add tanker to refuel_tankers
-
+    add tanker to regeneration_tankers list
     upgrade regeneration_rate of the hub
 
-    for tanker in entities:
-        move tankers towards different peaks
+    for all the tankers in regeneration_tankers list
+        if tanker available_energy different from its storage_capacity
+            move tankers towards random peaks
+            absorb the peak
+        else
+            move tanker to the hub
+            transfer its energy to the hub
 
-        for peaks in entities:
-            if distance between tanker and peak <= 1:
-                absorb energy from the peak
-        end for
-
-        if distance between tanker and hub <= 1:
-            transfer all the energy from the tanker to the hub
     end for
-
-    turn += 1
 
 end while
 
 #SECOND PHASE
 
-while moving_cost > 5 and regeneration rate == 50:
-    create cruiser
-    add cruiser to attack_cruisers
-    upgrade the moving cost of the cruisers
+initialize turn to 0
 
-    for tanker in refuel_tankers:
+while (turn < 5 or fire_range < 5) and regeneration_rate == 50
 
-        if tanker available_energy > 0:
-            move tanker towards hub
+    create tanker
+    transfer energy to the hub
+    upgrade fire_range
 
-            if distance between tanker and hub <= 1:
-                transfer all the energy from the tanker to the hub
+    for all the tankers
 
-            end if
-        end if
+        if tanker available_energy different from its storage_capacity
+            move tankers towards random peaks
+            absorb the peak
 
-        else:
-            move tanker towards different peaks
-            #il faudrait parcourir tous les peaks et vérifier quel tanker en est le plus proche pour bien répartir les transferts d'énergie
-
-            if distance between tanker and peak <= 1:
-                absorb energy from the peak
-
-            end if
-        end else
     end for
-            
-    for tanker in attack_tankers:
 
-        if tanker available_energy > 0:
-            move tanker towards the closest cruiser
+while (moving_cost > 5 or turn < 20) and  regeneration rate == 50 and fire_range == 5
 
-            if distance between tanker and the closest cruiser <= 1:
-                transfer all the energy from the tanker to the cruiser
+    if turn is pair
+        create 2 cruisers
+        add cruiser to attack_cruisers list
+    else
+        create one cruiser
+        upgrade the moving cost of the cruisers
 
-            end if
-        end if
+    for all the tankers in regeneration_tankers list
 
-        else:
-            move tanker towards different peaks
-            #il faudrait parcourir tous les peaks et vérifier quel tanker en est le plus proche pour bien répartir les transferts d'énergie
+        if tanker available_energy different from its storage_capacity
+            move tankers towards random peaks
+            absorb the peak
+        else
+            move tanker to the hub
+            transfer its energy to the hub
 
-            if distance between tanker and peak <= 1:
-                absorb energy from the peak
+    end for
 
-            end if
-        end else
+    for all the tankers not in regeneration_tankers list
+
+        if the available energy of the tanker equals to its storage_capacity
+            move tanker towards the cruiser which has the less available_energy
+            transfer energy to the cruiser
+
+        else
+            move each tanker towards a differenet peak
+            absorb energy from the peak
+
+    end for
+
+    for all the cruisers in attack_cruisers list
+        move the cruiser towards hub
+        attack the hub if it's possible
+
     end for
 
 #THIRD PHASE
 
+while fire_range == 5 and moving_cost == 5 and regeneration_rate == 50 and length of the defense_cruisers list < 15:
+
+    if turn is pair
+        create 2 cruisers
+        add the two cruisers in the defense_cruisers list
+
+        if the length of defense_cruisers list <=10:
+            for cruisers in defense_cruisers list
+                move the cruiser on a case where the distance with the hub equals 4
+                if there is another defense cruiser on the same case as the cruiser
+                    move the cruiser to another case
+        else
+            for cruiser in cruisers_list:
+                if distance between hub and cruiser != 4
+                    move cruiser to a case where the distance equals 5
+
+    else
+        create a tanker
+        add tanker to defense_tankers list
+
+    for cruiser in defense_cruisers list
+        if an opponent cruiser is in its fire_range
+            attack this cruiser
+
+# FOURTH PHASE
